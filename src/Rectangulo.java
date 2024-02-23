1b02b9b2246e48b9d388f8b26a1a27ed62f4ffef
Dorian Yael <yaelvillalva256@gmail.com>
2024-02-23 00:33:17 -0600

    complemetando_class

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -1,29 +1,41 @@
+import java.util.Scanner;
+
 public class Rectangulo {
     Integer base;
     Integer altura;
     String nombre;
 
-    Rectangulo(String nombre) {
+    Rectangulo(String nombre ) {
+
+        this.nombre = nombre;
+
     }
 
     Rectangulo(String nombre, int base, int altura) {
+        this.nombre=nombre;
+        this.base=base;
+        this.altura=altura;
     }
 
     public void setBase(int base) {
+       this.base=base;
+
     }
 
     public Integer getBase() {
-        return null;
+        return base;
     }
 
     public void setAltura(int altura) {
+        this.altura=altura;
     }
 
     public Integer getAltura() {
-        return null;
+        return altura;
     }
 
     public Integer obtenerArea() {
-        return null;
+        int area=base*altura;
+        return area;
     }
 }
